ef96a203a46e8eab1b920a1ded2961b31f7d66fd
Yuna Hsu <yunhsu473@gmail.com>
2023-05-04 18:27:12 +0800

    chore: merge main branch into dev branch

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,10 @@ import v1Routes from "./v1";
 
 const router = Router();
 
+router.get("/", (_req, res) => {
+  console.log("Hello World!");
+  res.send("Hello World!");
+});
 router.use("/api/v1", v1Routes);
 
 export default router;
