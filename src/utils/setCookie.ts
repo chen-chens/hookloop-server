c8a1e3b2e554f72c1ef2c9d8aa169ffa525a3ec7
Joanna.chen <joannachenchencom@gmail.com>
2023-05-09 21:33:32 +0800

    fix(setcookie): revise cookie expires

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -4,7 +4,7 @@ import HOOKLOOP_TOKEN from "@/const";
 
 const setCookie = (res: Response, token: string) => {
   res.cookie(HOOKLOOP_TOKEN, token, {
-    expires: new Date(Date.now() + 10 * 60), // 10 min for testing
+    expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 10 min for testing
     httpOnly: true,
     secure: true,
   });
