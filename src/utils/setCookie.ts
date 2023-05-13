8e5d0c5b9f3210b1ff69da6b61be47e78e7aa4cb
Joanna.chen <joannachenchencom@gmail.com>
2023-05-13 15:32:37 +0800

    fix: update setCookie cookie domain with subdomain

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -3,7 +3,7 @@ import { Response } from "express";
 import HOOKLOOP_TOKEN from "@/config/const";
 
 const setCookie = (res: Response, token: string) => {
-  const domain = process.env.NODE_ENV === "production" ? ".onrender.com" : ".localhost";
+  const domain = process.env.NODE_ENV === "production" ? ".hookloop-client.onrender.com" : ".localhost";
   res.cookie(HOOKLOOP_TOKEN, token, {
     expires: new Date(Date.now() + 24 * 2 * 60 * 60 * 1000),
     httpOnly: true,
