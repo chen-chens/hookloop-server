8f7b8435a382bf8b9c81906c5de5895d5ca0d416
jason <yulaie1012@gmail.com>
2023-05-14 00:48:04 +0800

    revert: restore initial settings

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -6,16 +6,12 @@ const setCookie = (res: Response, token: string) => {
   // const domain = process.env.NODE_ENV === "production" ? "https://hookloop-client.onrender.com/" : ".localhost";
   res.cookie(HOOKLOOP_TOKEN, token, {
     expires: new Date(Date.now() + 24 * 2 * 60 * 60 * 1000),
-    httpOnly: false,
-    // httpOnly: true,
+    httpOnly: true,
     secure: true,
     // domain, // accept domain for setting cookie
     path: "/",
     sameSite: "none", // accept setting cookie for cross-origin
   });
-
-  res.cookie("test1", "test");
-  res.cookie("HOOKLOOP_TOKEN", token);
 };
 
 export default setCookie;
