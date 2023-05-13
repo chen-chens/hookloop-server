27762b132541961f75cfedbe989fa12bc9e717f0
Yuna Hsu <yunhsu473@gmail.com>
2023-05-13 16:54:03 +0800

    fix: try remove domain restrict for cookie setting

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -3,12 +3,12 @@ import { Response } from "express";
 import HOOKLOOP_TOKEN from "@/config/const";
 
 const setCookie = (res: Response, token: string) => {
-  const domain = process.env.NODE_ENV === "production" ? "https://hookloop-client.onrender.com/" : ".localhost";
+  // const domain = process.env.NODE_ENV === "production" ? "https://hookloop-client.onrender.com/" : ".localhost";
   res.cookie(HOOKLOOP_TOKEN, token, {
     expires: new Date(Date.now() + 24 * 2 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
-    domain, // accept domain for setting cookie
+    // domain, // accept domain for setting cookie
     path: "/",
     sameSite: "none", // accept setting cookie for cross-origin
   });
