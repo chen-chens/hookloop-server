dffd74bb79e8ba6c0de4c73e28a42871f01184ca
jason <yulaie1012@gmail.com>
2023-05-14 00:09:15 +0800

    test: no secure

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -7,7 +7,7 @@ const setCookie = (res: Response, token: string) => {
   res.cookie(HOOKLOOP_TOKEN, token, {
     expires: new Date(Date.now() + 24 * 2 * 60 * 60 * 1000),
     // httpOnly: true,
-    secure: true,
+    // secure: true,
     domain, // accept domain for setting cookie
     path: "/",
     sameSite: "none", // accept setting cookie for cross-origin
