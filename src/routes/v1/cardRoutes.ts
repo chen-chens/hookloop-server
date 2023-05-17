f2ca35fb7f488bb434b28662a912228c6082b4a4
yunhsuan <yunhsuan2017@gmail.com>
2023-05-17 17:23:41 +0800

    feat: add getCardById

--- src/routes/v1/cardRoutes.ts
+++ src/routes/v1/cardRoutes.ts
@@ -7,6 +7,7 @@ const router = Router();
 
 router.post("/", asyncWrapper(cardControllers.createCard));
 router.get("/:id", asyncWrapper(cardControllers.getCardById));
+router.patch("/:id", asyncWrapper(cardControllers.updateCard));
 router.patch("/:id/archive", asyncWrapper(cardControllers.archiveCard));
 router.patch("/move", asyncWrapper(cardControllers.moveCard));
 
