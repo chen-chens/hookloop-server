7054c4447239c9a247033b13dc6acace602ef633
Emi <bell881122@gmail.com>
2023-05-23 22:36:49 +0800

    feat: add userRoutes OPTIONS API

--- src/routes/v1/userRoutes.ts
+++ src/routes/v1/userRoutes.ts
@@ -11,6 +11,11 @@ router.get("", asyncWrapper(userControllers.getAllUsers));
 router.get("/me", verifyTokenMiddleware, asyncWrapper(userControllers.getUserById));
 router.post("", verifyUserInputMiddleware, asyncWrapper(userControllers.createUser));
 router.patch("/me", asyncWrapper(userControllers.updateUser));
+router.options("", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
+  res.send({ status: "success" });
+});
 router.patch("/me/password", asyncWrapper(userControllers.updatePassword));
 
 router.delete("", asyncWrapper(userControllers.deleteAllUsers));
