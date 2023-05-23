0749de7a6ba0852194cae5990bdf7e5b65448102
Emi <bell881122@gmail.com>
2023-05-23 23:09:52 +0800

    feat: add userRoutes OPTIONS API-4

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
-  origin: ["*"],
+  origin: ["http://localhost:3000", "https://hookloop-client.onrender.com"],
   credentials: true,
   preflightContinue: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
