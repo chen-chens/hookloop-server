61658e4ee939c92cb310276471edc6fad6fdc10e
Emi <bell881122@gmail.com>
2023-05-24 09:29:16 +0800

    fix: update CORS setttings-2

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -3,6 +3,7 @@ import { CorsOptions } from "cors";
 const corsOptions: CorsOptions = {
   origin: ["http://localhost:3000", "https://hookloop-client.onrender.com"],
   credentials: true,
+  preflightContinue: true,
 };
 
 export default corsOptions;
