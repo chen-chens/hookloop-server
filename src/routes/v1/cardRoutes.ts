ae70afe34ff775b29c3972ca5d6dadf784619bdf
yunhsuan <yunhsuan2017@gmail.com>
2023-06-12 10:38:41 +0800

    fix: fix Provisional headers are shown problem

--- src/routes/v1/cardRoutes.ts
+++ src/routes/v1/cardRoutes.ts
@@ -5,26 +5,38 @@ import { asyncWrapper, validate, verifyTokenMiddleware, verifyUploadMiddleware }
 import { cardValidator } from "@/utils";
 
 const router = Router();
-router.use("/",verifyTokenMiddleware);
 
-router.post("/", validate(cardValidator.createCard, "CREATE"), asyncWrapper(cardControllers.createCard));
+router.post(
+  "/",
+  verifyTokenMiddleware,
+  validate(cardValidator.createCard, "CREATE"),
+  asyncWrapper(cardControllers.createCard),
+);
 
-router.get("/:id", validate(cardValidator.getCardById, "READ"), asyncWrapper(cardControllers.getCardById));
+router.get(
+  "/:id",
+  verifyTokenMiddleware,
+  validate(cardValidator.getCardById, "READ"),
+  asyncWrapper(cardControllers.getCardById),
+);
 
 router.patch(
   "/:kanbanId/:id/update",
+  verifyTokenMiddleware,
   validate(cardValidator.updateCard, "UPDATE"),
   asyncWrapper(cardControllers.updateCard),
 );
 router.patch(
   "/:id/isArchived",
+  verifyTokenMiddleware,
   validate(cardValidator.archiveCard, "DELETE"),
   asyncWrapper(cardControllers.archiveCard),
 );
-router.patch("/move", asyncWrapper(cardControllers.moveCard));
+router.patch("/move", verifyTokenMiddleware, asyncWrapper(cardControllers.moveCard));
 
 router.post(
   "/:cardId/attachment",
+  verifyTokenMiddleware,
   validate(cardValidator.addAttachment, "UPLOAD"),
   // .single("file") 限制處理單一檔案，但若無檔案不會報錯
   verifyUploadMiddleware.attachment.single("file"),
@@ -32,26 +44,40 @@ router.post(
 );
 router.delete(
   "/:cardId/attachment/:attachmentId",
+  verifyTokenMiddleware,
   validate(cardValidator.deleteAttachment, "DELETE"),
   asyncWrapper(cardControllers.deleteAttachment),
 );
 
-router.get("/:cardId/comment", validate(cardValidator.getComments, "READ"), asyncWrapper(cardControllers.getComments));
+router.get(
+  "/:cardId/comment",
+  verifyTokenMiddleware,
+  validate(cardValidator.getComments, "READ"),
+  asyncWrapper(cardControllers.getComments),
+);
 
-router.post("/:cardId/comment", validate(cardValidator.addComment, "CREATE"), asyncWrapper(cardControllers.addComment));
+router.post(
+  "/:cardId/comment",
+  verifyTokenMiddleware,
+  validate(cardValidator.addComment, "CREATE"),
+  asyncWrapper(cardControllers.addComment),
+);
 
 router.patch(
   "/:cardId/comment/:commentId",
+  verifyTokenMiddleware,
   validate(cardValidator.updateComment, "UPDATE"),
   asyncWrapper(cardControllers.updateComment),
 );
 router.patch(
   "/:cardId/comment/:commentId/isArchived",
+  verifyTokenMiddleware,
   validate(cardValidator.archiveComment, "DELETE"),
   asyncWrapper(cardControllers.archiveComment),
 );
 router.get(
   "/:cardId/comment/:commentId",
+  verifyTokenMiddleware,
   validate(cardValidator.getCommentHistory, "READ"),
   asyncWrapper(cardControllers.getCommentHistory),
 );
