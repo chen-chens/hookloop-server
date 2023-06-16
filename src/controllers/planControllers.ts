6fc3674b6cf6c2e0e4e1f836bd50f9f55b17657f
Joanna.chen <joannachenchencom@gmail.com>
2023-06-16 08:17:27 +0800

    test: test for paymentNotify 4

--- src/controllers/planControllers.ts
+++ src/controllers/planControllers.ts
@@ -79,47 +79,47 @@ const createOrderForPayment = async (req: IPlanOrderRequest, res: Response, next
   });
 };
 
-const paymentNotify = async (req: Request, res: Response, next: NextFunction) => {
+const paymentNotify = async (req: Request) => {
   try {
-    const { PAY_MERCHANT_ID, PAY_VERSION, PAY_RETURN_URL, PAY_NOTIFY_URL, PAY_HASH_IV, PAY_HASH_KEY } = process.env;
+    // const { PAY_MERCHANT_ID, PAY_VERSION, PAY_RETURN_URL, PAY_NOTIFY_URL, PAY_HASH_IV, PAY_HASH_KEY } = process.env;
     console.log("🚀 ======================= 進入藍新回傳 Notify =====================");
     console.log("🚀 ~ =======================  paymentNotify req.body :", req.body);
 
-    if (!PAY_MERCHANT_ID || !PAY_VERSION || !PAY_RETURN_URL || !PAY_NOTIFY_URL || !PAY_HASH_IV || !PAY_HASH_KEY) {
-      forwardCustomError(next, StatusCode.INTERNAL_SERVER_ERROR, ApiResults.UNEXPECTED_ERROR);
-      return;
-    }
-    // 解密資料，核對 產品編號是否一致
-    const key = CryptoJS.enc.Utf8.parse(PAY_HASH_KEY); // 先轉成 CryptoJS 可接受加密格式：WordArray
-    const iv = CryptoJS.enc.Utf8.parse(PAY_HASH_IV);
-    const ciphertext = CryptoJS.enc.Hex.parse(req.body.TradeInfo as string);
-    const decrypted = CryptoJS.AES.decrypt(ciphertext.toString(), key, {
-      iv,
-      padding: CryptoJS.pad.Pkcs7,
-    });
-    const decryptedWithoutPadding = CryptoJS.enc.Utf8.stringify(decrypted).replace(/\0+$/, "");
-    console.log("🚀 ~ file: planControllers.ts:95 ~ paymentNotify ~ decryptedWithoutPadding:", decryptedWithoutPadding);
-    const [returnInfo] = decryptedWithoutPadding.split("&").map((item) => {
-      const [prop, value] = item.split("=");
-      return { [prop]: value };
-    });
-    if (returnInfo.status !== "SUCCESS") {
-      forwardCustomError(next, StatusCode.BAD_REQUEST, ApiResults.FAIL_TO_PAY);
-      return;
-    }
+    // if (!PAY_MERCHANT_ID || !PAY_VERSION || !PAY_RETURN_URL || !PAY_NOTIFY_URL || !PAY_HASH_IV || !PAY_HASH_KEY) {
+    //   forwardCustomError(next, StatusCode.INTERNAL_SERVER_ERROR, ApiResults.UNEXPECTED_ERROR);
+    //   return;
+    // }
+    // // 解密資料，核對 產品編號是否一致
+    // const key = CryptoJS.enc.Utf8.parse(PAY_HASH_KEY); // 先轉成 CryptoJS 可接受加密格式：WordArray
+    // const iv = CryptoJS.enc.Utf8.parse(PAY_HASH_IV);
+    // const ciphertext = CryptoJS.enc.Hex.parse(req.body.TradeInfo as string);
+    // const decrypted = CryptoJS.AES.decrypt(ciphertext.toString(), key, {
+    //   iv,
+    //   padding: CryptoJS.pad.Pkcs7,
+    // });
+    // const decryptedWithoutPadding = CryptoJS.enc.Utf8.stringify(decrypted).replace(/\0+$/, "");
+    // console.log("🚀 ~ file: planControllers.ts:95 ~ paymentNotify ~ decryptedWithoutPadding:", decryptedWithoutPadding);
+    // const [returnInfo] = decryptedWithoutPadding.split("&").map((item) => {
+    //   const [prop, value] = item.split("=");
+    //   return { [prop]: value };
+    // });
+    // if (returnInfo.status !== "SUCCESS") {
+    //   forwardCustomError(next, StatusCode.BAD_REQUEST, ApiResults.FAIL_TO_PAY);
+    //   return;
+    // }
 
-    const updateDbTradeRecord = Plan.findOneAndUpdate(
-      { merchantOrderNo: returnInfo.merchantOrderNo },
-      {
-        status: returnInfo.status === "SUCCESS" ? "PAID" : "UN-PAID",
-        payMethod: "WEBATM",
-      },
-    );
-    if (!updateDbTradeRecord) {
-      forwardCustomError(next, StatusCode.INTERNAL_SERVER_ERROR, ApiResults.FAIL_UPDATE);
-      return;
-    }
-    sendSuccessResponse(res, ApiResults.SUCCESS_TO_PAY);
+    // const updateDbTradeRecord = Plan.findOneAndUpdate(
+    //   { merchantOrderNo: returnInfo.merchantOrderNo },
+    //   {
+    //     status: returnInfo.status === "SUCCESS" ? "PAID" : "UN-PAID",
+    //     payMethod: "WEBATM",
+    //   },
+    // );
+    // if (!updateDbTradeRecord) {
+    //   forwardCustomError(next, StatusCode.INTERNAL_SERVER_ERROR, ApiResults.FAIL_UPDATE);
+    //   return;
+    // }
+    // sendSuccessResponse(res, ApiResults.SUCCESS_TO_PAY);
     // 如果資料一致，就可以更新到 DB
   } catch (err) {
     console.log("XXXXXXXXXXX 藍新回傳 Notify 失敗XXXXXXXXXXXX", err);
