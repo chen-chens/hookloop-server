a0b56c844ce3417fb8e08b033dcafde3c460a7f6
Yuna Hsu <yunhsu473@gmail.com>
2023-06-30 00:38:48 +0800

    fix(admin): fix admin register repulcated path

--- src/routes/v1/adminRoutes.ts
+++ src/routes/v1/adminRoutes.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.post("/users", asyncWrapper(adminControllers.getUsers));
 router.get("/users/:id", asyncWrapper(adminControllers.getUserById));
 router.patch("/users/:id", asyncWrapper(adminControllers.updateUserById));
-router.post("/users", asyncWrapper(adminControllers.register));
+router.post("/users/register", asyncWrapper(adminControllers.register));
 router.post("/login", asyncWrapper(adminControllers.login));
 router.get("/plans/:userId", asyncWrapper(adminControllers.getPlansByUserId));
 router.post("/plans/user", asyncWrapper(adminControllers.getPlans));
